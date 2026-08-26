e69978f0ca1d7c6cf30964858e96e8fb1d0d0093
Sammed Sunil Admuthe <36372399+SammedAdmuthe@users.noreply.github.com>
2026-08-25 20:58:28 -0500

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+
+        res = []
+
+        def explore(count_open, count_close, constructed):
+
+
+
+            if count_open == n and count_close == n:
+                print(constructed)
+                res.append("".join(constructed))
+                return
+            if count_open < count_close or count_open> n or count_close > n:
+                return
+            
+
+            constructed.append('(')
+            explore(count_open+1, count_close, constructed)
+            constructed.pop()
+            constructed.append(')')
+            explore(count_open, count_close+1, constructed)
+            constructed.pop()
+
+
+        explore(0, 0, [])
+        return res
